e6ee30dbae9a8237a7415a3ea7f12965e71d8cd2
st185729 <118833369+st185729@users.noreply.github.com>
2024-05-07 13:46:36 +0200

    test2

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
         public static void main(String[] args){
-            DateiLesen.ladeDatei("ProPro.germany.fmi");
+            DateiLesen.ladeDatei("germany.fmi");
         }
     }
 
