34865bb4cc240df067feb5b57f3322744665be65
st185729 <118833369+st185729@users.noreply.github.com>
2024-05-07 13:21:57 +0200

    test

---
+++ DateiLesen.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DateiLesen {
+    private static void ladeDatei(String datName) {
+
+        File file = new File(datName);
+
+        if (!file.canRead() || !file.isFile())
+            System.exit(0);
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(datName));
+            String zeile = null;
+            while ((zeile = in.readLine()) != null) {
+                System.out.println("Gelesene Zeile: " + zeile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null)
+                try {
+                    in.close();
+                } catch (IOException e) {
+                }
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public class Main {
+        public static void main(String[] args){
+            public DateiZeilenweiseAuslesenClass reader = new DateiZeilenweiseAuslesenClass();
+            raeder.ladeDatei(germany.fmi);
+        }
+    }
+}
